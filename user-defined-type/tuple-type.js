bbbed93144999f50e7fa0400e9ebd5a23d092eb5
srs-sieam <srssieam@gmail.com>
2024-01-10 17:21:08 +0600

    tuple data type

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,7 @@
+// mixed data - key, value
+var user;
+user = [101, "sieam"];
+user.push(102, "roni");
+console.log(user);
+// errors
+// user = ["101", "sieam"]
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,10 @@
+// mixed data - key, value
+let user: [number, string] 
+user = [101, "sieam"]
+
+user.push(102, "roni")
+
+console.log(user);
+
+// errors
+// user = ["101", "sieam"]
