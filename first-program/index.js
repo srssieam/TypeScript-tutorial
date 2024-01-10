c0835c0a63237795c4baa753b8262fa36993784e
srs-sieam <srssieam@gmail.com>
2024-01-10 12:51:41 +0600

    typescript detects error before running the code

--- first-program/index.js
+++ first-program/index.js
@@ -1,3 +1,7 @@
-var userName;
-userName = "sieam";
-console.log(userName);
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
+// errors
+addNumbers(20);
+addNumbers(20, "30"); // typescript detect error before running the code
--- first-program/index.ts
+++ first-program/index.ts
@@ -1,3 +1,8 @@
-let userName: string;
-userName = "sieam";
-console.log(userName);
+function addNumbers(num1: number, num2: number) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
+
+// errors
+addNumbers(20);
+addNumbers(20, "30");  // typescript detect error before running the code
